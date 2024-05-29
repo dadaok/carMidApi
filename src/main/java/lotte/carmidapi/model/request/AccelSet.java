87c7b61b9a068f604b5f65dedca6b2b4640d50e5
hipzil <sm4m86@gmail.com>
2024-05-29 21:52:02 +0900

    수정

--- src/main/java/lotte/carmidapi/model/request/AccelSet.java
+++ src/main/java/lotte/carmidapi/model/request/AccelSet.java
@@ -1,5 +1,6 @@
 package lotte.carmidapi.model.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lotte.carmidapi.common.AsIsPropertyNamingStrategy;
 import lombok.Getter;
@@ -7,6 +8,7 @@ import lombok.Getter;
 @Getter
 @JsonNaming(AsIsPropertyNamingStrategy.class)
 public class AccelSet {
+    @JsonProperty("long")
     private int Long;
     private int lat;
     private int vert;
--- src/test/java/lotte/carmidapi/CarMidApiApplicationTests.java
+++ src/test/java/lotte/carmidapi/CarMidApiApplicationTests.java
@@ -9,5 +9,4 @@ class CarMidApiApplicationTests {
     @Test
     void contextLoads() {
     }
-
 }
