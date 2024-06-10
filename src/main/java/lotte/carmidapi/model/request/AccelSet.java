32991ce625be31e7996f2394cbfa2747aebd0ebd
hipzil <sm4m86@gmail.com>
2024-06-10 09:03:03 +0900

    수정

--- src/main/java/lotte/carmidapi/model/request/AccelSet.java
+++ src/main/java/lotte/carmidapi/model/request/AccelSet.java
@@ -9,13 +9,12 @@ import lombok.Getter;
 @Getter
 @JsonNaming(AsIsPropertyNamingStrategy.class)
 public class AccelSet {
-    @JsonProperty("long")
+    @JsonProperty("Long")
     @Schema(example = "15")
     private int Long;
     @Schema(example = "16")
     private int lat;
     @Schema(example = "17")
-    private int vert;
-    @Schema(example = "18")
+    private int vert;    @Schema(example = "18")
     private int yaw;
 }
